86e559f61dd436a23ae2310cec9442a777956545
erlonbie <erlonminievix@gmail.com>
2021-05-25 12:48:44 -0400

    ajuste classes

--- TP1-Implementacao/src/Apartamento.java
+++ TP1-Implementacao/src/Apartamento.java
@@ -1,11 +1,11 @@
 
-public class Kitchenette extends Imovel {
+public class Apartamento extends Imovel {
 
-	public Kitchenette(String categoria, String endereco, double area, double custo, int qntQuartos, int qntSuites,
+	public Apartamento(String categoria, String endereco, double area, double custo, int qntQuartos, int qntSuites,
 			int qntVagasEstacionamento, boolean piscina, boolean churrasqueira, boolean playground, boolean alugado) {
 		super(categoria, endereco, area, custo, qntQuartos, qntSuites, qntVagasEstacionamento, piscina, churrasqueira,
 				playground, alugado);
 		// TODO Auto-generated constructor stub
 	}
-	
+
 }
