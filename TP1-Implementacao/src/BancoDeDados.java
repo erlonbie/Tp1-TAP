4667ade96f3afffd6e7922c912f8d7e642d776b7
erlonbie <erlonminievix@gmail.com>
2021-05-25 23:53:53 -0400

    adicionada classe BancoDeDados

---
+++ TP1-Implementacao/src/BancoDeDados.java
@@ -0,0 +1,33 @@
+import java.sql.*;
+
+public class BancoDeDados {
+	private static String url = "jdbc:mysql://localhost:3306/CitacoesBD";
+	private static String user = "citacoes_admin";
+	private static String pass = "Teste123";
+	protected static Connection conexao = null;
+	
+	public BancoDeDados() {
+		if(conexao == null) {
+			conecta();
+		}
+	}
+	
+	private static boolean conecta() {
+		try {
+			conexao = DriverManager.getConnection(url, user, pass);
+			return true;
+		}
+		catch (SQLException e) {
+			return false;
+		}
+	}
+	
+	public static boolean desconecta() {
+		try {
+			conexao.close();
+			return true;
+		}catch(SQLException e) {
+			return false;
+		}
+	}
+}
