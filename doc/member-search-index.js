335178e4bb65ae29c6ac8673c8c3d6d28951b484
erlonbie <erlonminievix@gmail.com>
2021-06-06 14:48:34 -0400

    javadoc e script_db

---
+++ doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"VisualMain","l":"main(String[])","url":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"VisualMain","l":"VisualMain()","url":"%3Cinit%3E()"}]
